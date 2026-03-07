b1686fab009c615d238bd0e1f67f43de076591a0
Nguyenquan2807 <nguyenhuuquan28072004@gmail.com>
2026-03-07 19:55:51 +0700

    create spring boot backend project

---
+++ backend/sportwearshop/src/main/java/com/nguyenhuuquan/sportwearshop/SportwearshopApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenhuuquan.sportwearshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportwearshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportwearshopApplication.class, args);
+	}
+
+}
---
+++ backend/sportwearshop/src/test/java/com/nguyenhuuquan/sportwearshop/SportwearshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenhuuquan.sportwearshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportwearshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
